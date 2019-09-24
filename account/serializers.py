b4c52f237fd37001ddf0e5326fb923c2ce79feb3
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-09-24 13:24:19 +0330

    add user and login serializers

---
+++ account/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class UserSingInSerializer(serializers.Serializer):
+    username = serializers.CharField(label=_("Username"))
+    password = serializers.CharField(label=_("Password"),
+                                     style={'input_type':'password'},
+                                     trim_whitespace=False)
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id','username','email')
