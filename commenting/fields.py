009062b92dd075b7189d3516e8b3e106933718ce
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-10-04 17:08:51 +0330

    fix bugs of domain name field

--- commenting/fields.py
+++ commenting/fields.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from .validators import domain_name_validator
 import re
 
 
 class DomainNameField(models.CharField):
-    description = 'Domain name field'
-    default_validators = [domain_name_validator]
+    description = _('Domain name field')
+    empty_strings_allowed = False
+    default_validators = [domain_name_validator, ]
 
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         kwargs['max_length'] = kwargs.get('max_length', 72)
         super(DomainNameField, self).__init__(*args, **kwargs)
 
-    def clean(self, value, model_instance):
-        def check(val):
+    def to_python(self, value):
+        def convert(val):
+            domain_name_validator(value)
             pattern = re.compile(r"https?://(www\.)?")
             result = pattern.sub('', val).strip().strip('/')
             return result
-        value = check(value)
-        model_instance.domain = value
-        return value
+        if isinstance(value, str) or value is None:
+            return convert(value)
+        return convert(str(value))
