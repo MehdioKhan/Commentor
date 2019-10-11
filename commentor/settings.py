79d4f7da957c4a06f4fa0bd6a758590e1587db67
Khan <mehdiokhan@gmail.com>
2019-10-11 18:52:49 +0330

    start management app

--- commentor/settings.py
+++ commentor/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account',
     'commenting',
+    'management',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
